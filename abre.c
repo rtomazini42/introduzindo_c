e0dbe1f01aade236bba14ba04b600f010c1fa2d6
Renan Tomazini <renantomazini@gmail.com>
2018-04-20 17:38:42 -0300

    Abridor de arquivos e imprimi fudidaço

---
+++ abre.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+char palavra[20];
+
+void abrir(){
+
+FILE* f;
+f = fopen("letras.txt.txt","r"); //(ah nao)
+if(f == 0) {
+        printf("Banco de dados de palavras nao disponivel\n\n");
+        exit(1);
+    }
+
+fscanf(f,"%s", palavra);
+printf("\n aqui! \n %s", palavra);
+fclose(f);
+}
+
+
+
+int main()
+{
+    printf("Hello world!\n");
+    abrir();
+    return 0;
+}
