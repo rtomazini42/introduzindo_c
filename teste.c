fdf8ce2eab7d5ef5ea6988df3ce8096bab0b7ec6
Renan Tomazini <renantomazini@gmail.com>
2018-04-23 14:33:57 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -2,9 +2,21 @@
 #include <stdlib.h>
 
 int main(){
-int* v = malloc(8);
+int* v = malloc(sizeof(int*) * 5);
+for(int i = 0; i <5; i++){
+    v[i] = malloc(sizeof(int)*10);
+}
+
+v[0] = 10;
+v[1] = 12;
+
 *v = 10;
 printf("%d\n\n", *v);
+
+for(int i = 0; i < 5; i++){
+    free(v[i]);
+}
+
 free(v);
 
 }
