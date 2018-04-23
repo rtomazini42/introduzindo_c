4bb83227860313c0276fc53b1dea19437b2ba7e2
Renan Tomazini <renantomazini@gmail.com>
2018-04-23 18:23:01 -0200

    anda
    Agora o bixim anda

--- jogoPac.c
+++ jogoPac.c
@@ -4,6 +4,13 @@
 char** mapa;
 int linhas, colunas;
 
+void aloca_mapa(){
+mapa = malloc(sizeof(char*) * linhas);
+    for (int i = 0; i < linhas; i++){
+        mapa[i] = malloc(sizeof(char) * colunas + 1);
+    }
+
+}
 void libera_mapa(){
     for(int i = 0; i < linhas; i++){
         free(mapa[i]);
@@ -19,31 +26,68 @@ void abre_mapa(){
         exit(1);}
 
     fscanf(f,"%d %d", &linhas, &colunas);
-    /////////
-    mapa = malloc(sizeof(char*) * linhas);
-    for (int i = 0; i < linhas; i++){
-        mapa[i] = malloc(sizeof(char) * colunas + 1);
-    }
-
-    /////////
+    aloca_mapa();
 
     for(int i = 0; i < 5; i++){
         fscanf(f,"%s", mapa[i]);}
 
 
-    for(int i = 0; i < 5; i++){
-        printf("%s\n", mapa[i]);
-    }
 
-    libera_mapa();
+
     fclose(f);
 }
 
+int acabou(){
+    return 0;
+}
+
+void imprime_mapa(){
+for(int i = 0; i < 5; i++){
+        printf("%s\n", mapa[i]);
+    }
+}
+
+void move(char direcao){
+    int x,y;
+    for(int i = 0; i < linhas; i++){
+        for(int j = 0; j < colunas; j++){
+            if (mapa[i][j]== '@'){
+                x = i;
+                y = j;
+            }
+        }
+    }
+    switch(direcao){
+        case 'a':
+            mapa[x][y-1] = '@';
+            break;
+        case 'd':
+            mapa[x][y+1] = '@';
+            break;
+        case 's':
+            mapa[x+1][y] = '@';
+            break;
+        case 'w':
+            mapa[x-1][y] = '@';
+            break;
+
+    }
+    mapa[x][y] = '.';
+}
 
 int main()
 {
 
-    abre_mapa();
 
+    abre_mapa();
+    do{
+         imprime_mapa();
+         char comando;
+         scanf(" %c", &comando);
+         move(comando);
+
+    }while(!acabou());
+
+    libera_mapa();
 
 }
