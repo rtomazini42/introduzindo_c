cee18fd0fd9b4282f788efb4f426f2edb7702aea
Renan Tomazini <renantomazini@gmail.com>
2018-04-24 01:25:50 -0300

    Update cavalo.c

--- cavalo.c
+++ cavalo.c
@@ -3,8 +3,10 @@
 #include <time.h>
 
 
-int tabuleiro[7][7];
+int tabuleiro[8][8];
 int pecas = 8;
+
+
 
 void inicia_tab(){
 for(int i = 0; i < 8; i++){
@@ -19,11 +21,12 @@ for(int i = 0; i < 8; i++){
 void imprime_tab(){
 for(int i = 0; i < 8; i++){
     for(int j = 0 ; j < 8; j++){
-        printf(" %d ", tabuleiro[i][j]);
+        printf("[%d]", tabuleiro[i][j]);
     }
     printf("\n");
 
 }}
+
 void inimigos(){
     srand(time(0));
     int aleY = rand() % 8;
@@ -34,30 +37,45 @@ void inimigos(){
     else{
         pecas++;
     }
+
 }
 
 void cavalo(){
+    int cavX, cavY;
+    while (tabuleiro[cavX][cavY] != 0){
+        srand(time(0));
+        cavY = rand() % 8;
+        cavX = rand() % 8;
 
-    srand(time(0));
-    int aleY = rand() % 8;
-    int aleX = rand() % 8;
-    if (tabuleiro[aleX][aleY] == 0){
-        tabuleiro[aleX][aleY] = 5;
     }
-    
-
+    tabuleiro[cavX][cavY] = 5;
 }
+
+
+void comer(){
+    for(int i = 0; i < 8; i++){
+        for(int j = 0 ; j < 8; j++){
+            if(tabuleiro[j][i] == 5)
+                {
+                    printf("O cavalo esta no %d %d\n", j+1,i+1);
+
+
+
+                }
+            }
+
+}}
 
 int main()
 {   //int aux = 0;
+    printf("Pode demorar ate 13 segundos \n\n");
     inicia_tab();
-    //printf("%d", tabuleiro[0][0]);
     while (!pecas == 0){
       inimigos();
-      pecas--;
-
-    }
+      pecas--;}
     cavalo();
     imprime_tab();
+    printf("\n");
+    comer();
 
 }
