6fb9fa4084d7a055ba175dc21bea8b8b968b764d
Renan Tomazini <renantomazini@gmail.com>
2018-04-23 21:40:24 -0300

    Create cavalo.c

---
+++ cavalo.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int tabuleiro[7][7];
+int pecas = 8;
+
+void inicia_tab(){
+for(int i = 0; i < 8; i++){
+    for(int j = 0 ; j < 8; j++){
+        tabuleiro[i][j] = 0;
+    }
+
+}
+}
+
+
+void imprime_tab(){
+for(int i = 0; i < 8; i++){
+    for(int j = 0 ; j < 8; j++){
+        printf(" %d ", tabuleiro[i][j]);
+    }
+    printf("\n");
+
+}}
+void inimigos(){
+    srand(time(0));
+    int aleY = rand() % 8;
+    int aleX = rand() % 8;
+    if (tabuleiro[aleX][aleY] == 0){
+        tabuleiro[aleX][aleY] = 1;
+    }
+    else{
+        pecas++;
+    }
+}
+
+
+int main()
+{   //int aux = 0;
+    inicia_tab();
+    //printf("%d", tabuleiro[0][0]);
+    while (!pecas == 0){
+      inimigos();
+      pecas--;
+    }
+    imprime_tab();
+
+}
