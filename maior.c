ff6baeced262eafaf8ce3276e6734b84c8d77bfa
Renan Tomazini <renantomazini@gmail.com>
2018-04-25 20:07:18 -0300

    Create maior.c

---
+++ maior.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int maiorn = 0;
+    int v[] = {3,4,2,6,8};
+    for(int i = 0; i < 5; i++){
+        {if (v[i]> maiorn){
+            maiorn = v[i];
+
+        }
+
+    printf("%i\n", maiorn);
+        }}
+}
