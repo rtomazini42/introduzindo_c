2d53c57ae873d5681147a1502081175f07a22de0
Renan Tomazini <renantomazini@gmail.com>
2020-05-03 23:36:40 -0300

    Adicionando mais uma merda inutil
    Eu só queria brincar de música nesse pequeno exercício, mas descobri tanta variação de notas musicais produzidas de palacas de computadores que vou deixar esse código aqui como ode ao meu fracasso

---
+++ beeps.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h> //caso seja um sistema linux ultilize "//" para desconsiderar essa linha
+
+int main()
+{    Beep(392,350);
+
+     Beep(392,350);
+     Beep(392,350);
+     Beep(311,250);
+     Beep(466,25);
+     Beep(392,350);
+     Beep(311,250);
+     Beep(466,25);
+     Beep(392,700);
+     Beep(587,350);
+     Beep(587,350);
+     Beep(587,350);
+     Beep(622,250);
+     Beep(466,25);
+     Beep(370,350);
+     Beep(311,250);
+     Beep(466,25);
+     Beep(392,700);
+     Beep(784,350);
+     Beep(392,250);
+     Beep(784,350);
+     Beep(740,250);
+     Beep(392,250);
+     Beep(784,350);
+     Beep(740,250);
+     Beep(698,25);
+     Beep(669,25);
+     Beep(622,25);
+     Beep(659,50);
+     Beep(415,25);
+     Beep(554,350);
+     Beep(523,250);
+     Beep(494,25);
+     Beep(466,25);
+     Beep(440,25);
+     Beep(466,50);
+     Beep(311,25);
+     Beep(370,350);
+     Beep(311,250);
+     Beep(392,25);
+     Beep(466,350);
+     Beep(392,250);
+     Beep(587,700);
+     Beep(784,350);
+     Beep(392,250);
+     Beep(392,25);
+     Beep(784,350);
+     Beep(740,250);
+     Beep(698,25);
+     Beep(659,25);
+     Beep(622,25);
+     Beep(659,50);
+     Beep(415,25);
+     Beep(554,350);
+     Beep(523,250);
+     Beep(493,25);
+     Beep(466,25);
+     Beep(440,25);
+     Beep(466,50);
+     Beep(311,25);
+     Beep(392,250);
+     Beep(311,250);
+     Beep(466,25);
+     Beep(392,300);
+     Beep(311,250);
+     Beep(466,25);
+     Beep(392,700);
+
+
+
+     printf("Teste com beeps :D");
+
+    return main();
+}
